e1bd2aaf2dea2134964e88dc8e5ab6ae51ab1a67
Sang <nqsang@fossil.com>
2019-10-07 17:24:52 +0700

    msp430

---
+++ README.md
@@ -0,0 +1,5 @@
+# msp430
+debuger for msp430
+git clone https://github.com/dlbeer/mspdebug.git
+cd mspdebug/
+make
---
+++ msp430-gcc-8.2.0.52_linux64/set_up.sh
@@ -0,0 +1,3 @@
+DIR=/home/sang/my_things/draft/msp430/msp430-gcc-8.2.0.52_linux64
+echo $DIR
+export PATH=$DIR/bin:$PATH
